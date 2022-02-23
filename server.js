73f4316c68fb799866fa7049c65b5afb63d63378
Albert Chang <a89529294@hotmail.com>
2022-02-23 17:41:04 +0800

    finish ssr

--- server.js
+++ server.js
@@ -1,31 +1,41 @@
 import express from "express";
 import React from "react";
-import path from "path";
-import fs from "fs";
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
-import { App } from "./src/App";
+import { ServerStyleSheet } from "styled-components";
+import path from "path";
+import fs from "fs";
+import App from "./src/App";
 
 const app = express();
 
 app.use(express.static("./build", { index: false }));
 
 app.get("/*", (req, res) => {
+  const sheet = new ServerStyleSheet();
+
   const reactApp = renderToString(
-    <StaticRouter location={req.url}>
-      <App />
-    </StaticRouter>
+    sheet.collectStyles(
+      <StaticRouter location={req.url}>
+        <App />
+      </StaticRouter>
+    )
   );
 
   const templateFile = path.resolve("./build/index.html");
-  fs.readFile(templateFile, "utf-8", (err, data) => {
-    if (err) return res.status(500).send(err);
+  fs.readFile(templateFile, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
     return res.send(
-      data.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+      data
+        .replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+        .replace("{{styles}}", sheet.getStyleTags())
     );
   });
 });
 
 app.listen(8080, () => {
-  console.log("server is listening on port 8080");
+  console.log("Server is listening on port 8080");
 });
